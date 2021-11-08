7089b9eb3298ea73f36e201469178b2cb6178517
MDSELIM1 <selimakodo58@gmail.com>
2021-11-08 13:21:28 +0600

    add

--- src/components/HomePage/Contact/Contact.js
+++ src/components/HomePage/Contact/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
     return (
         <div className="contact-form-box py-4 mb-5">
             <h3 className="text-center py-5 text-uppercase main-title">Contact Us</h3>
-            <h1 className="text-center">Always Connect With Us</h1>
+            <h1 className="text-center contact-title">Always Connect With Us</h1>
             <div className="contact-form py-5">
             <form onSubmit={handleContactSubmit} className="text-center">
                 <input type="text" placeholder="Enter your name" />
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,73 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Footer.css';
 
 const Footer = () => {
     
     return (
-        <div>
-            
-        </div>
+        <footer className="footer-box p-5">
+            <Container fluid>
+                <Row>
+                    <Col lg={3} sm={12}>
+                        <div className="footer-signle left-single">
+                            <ul className="footer-services">
+                                <li>Emergency Dental Care</li>
+                                <li>Check Up</li>
+                                <li>Treatment and Personal Diseases</li>
+                                <li>Tooth Extraction</li>
+                                <li>Check up</li>
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col lg={3} sm={12}>
+                        <div className="footer-signle">
+                            <h3 className="footer-head-title">Services</h3>
+                            <ul className="footer-services">
+                                <li>Emergency Dental Care</li>
+                                <li>Check Up</li>
+                                <li>Treatment and Personal Diseases</li>
+                                <li>Tooth Extraction</li>
+                                <li>Check up</li>
+                                <li>Emergency Dental Care</li>
+                                <li>Check up</li>
+                                <li>Emergency Dental Care</li>
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col lg={3} sm={12}>
+                        <div className="footer-signle">
+                            <h3 className="footer-head-title">Oral Health</h3>
+                            <ul className="footer-services">
+                            <li>Tooth Extraction</li>
+                                <li>Check up</li>
+                                <li>Emergency Dental Care</li>
+                                <li>Check up</li>
+                                <li>Emergency Dental Care</li>
+                                <li>Emergency Dental Care</li>
+                                <li>Check Up</li>
+                                <li>Treatment and Personal Diseases</li>                                
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col lg={3} sm={12}>
+                        <div className="footer-signle">
+                            <h3 className="footer-head-title">Our Address</h3>
+                            <h5 className="footer-address">New York , 1010, Hudson Yards</h5>
+                            <ul className="social-link mb-3">
+                                <li><i class="fab fa-facebook-f"></i></li>
+                                <li><i class="fab fa-twitter"></i></li>
+                                <li><i class="fab fa-google-plus-g"></i></li>
+                            </ul>
+                            <h5 className="mt-5 call-now">Call Now</h5>
+                            <p className="common-button">+88566414564</p>
+                        </div>
+                    </Col>
+                </Row>
+                <div className="footer-copyright text-center mt-5">
+                    <p>&copy; All right reserved by 2021 || Selim</p>
+                </div>
+            </Container>
+        </footer>
     );
 };
 
